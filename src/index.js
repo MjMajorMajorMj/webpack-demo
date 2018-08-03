67e7106f01f293251e4c58b271cf738cf0d90da7
MjMajorMajorMj <techk8@gmail.com>
2018-08-02 17:39:22 -0700

    Added React

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-import $ from 'jquery';
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-$('#login').click(() => {
-    console.log('Button cliked')
-
-    import('./helpers').then( resp => {
-        console.log('Import Resp:', resp);
-        const { login, addToDom } = resp;
-
-        const result1 = login('BaconPancake','kingkong');
-
-        const result2 = login('HydroThunder','winner');
-
-        addToDom('h1', result1, 'blue');
-        addToDom('h1', result2, 'purple');
-    });
-});
-
-$('#load-image').click( async () => {
-    console.log('Load Image Clicked');
-
-    const resp = await import('./helpers');
-
-    const { addImg } = resp;
-
-    const src = await import('./demoimage.jpg');
-
-    addImg(src.default);
-});
+ReactDOM.render(
+    <h1>Welcome to React!</h1>,
+    document.getElementById('root')
+);
